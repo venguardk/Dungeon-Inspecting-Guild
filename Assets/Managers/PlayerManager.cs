08c350a19a114bca2693af1571fe936c2070936f
scso-ucsc <scso@ucsc.edu>
2025-01-25 13:58:49 -0800

    Adding Player Movement and Attack

--- Assets/Managers/PlayerManager.cs
+++ Assets/Managers/PlayerManager.cs
@@ -2,20 +2,85 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    //This movement script is based off of Introduction to Isometric Movement & Animation (8 directions) in Unity - https://youtu.be/UM9BSyGyGf0?si=Znou_TFHG17EaEHb
+    private Vector2 movementInput, movementVelocity, playerDirection;
     private Rigidbody2D rb;
-    private float moveHorizontal, moveVertical;
+    private SpriteRenderer sr;
     [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private GameObject attackHitBox;
+    [SerializeField] private float attackDuration = 0.1f, attackRange = 1f;
+    private bool isAttacking;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        playerDirection = Vector2.down;
+        attackHitBox.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (isAttacking)
+        {
+            movementInput = Vector2.zero;
+            return; //Stops player from moving while attacking
+        }
+
+        movementInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        if (movementInput != Vector2.zero)
+        {
+            playerDirection = movementInput;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isAttacking == false)
+        {
+            Attack();
+        }
     }
 
     private void FixedUpdate()
     {
-        moveHorizontal = Input.GetAxis("Horizontal") * moveSpeed;
-        moveVertical = Input.GetAxis("Vertical") * moveSpeed;
-        rb.linearVelocity = new Vector2(moveHorizontal, moveVertical);
+        if (isAttacking)
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+        else
+        {
+            movementVelocity = movementInput * moveSpeed;
+            rb.linearVelocity = movementVelocity;
+            FlipSprite();
+        }
+    }
+
+    private void Attack()
+    {
+        isAttacking = true;
+        Vector3 direction = playerDirection;
+        attackHitBox.transform.position = transform.position + direction * attackRange;
+
+        attackHitBox.SetActive(true);
+        Invoke(nameof(EndAttack), attackDuration); //Calls EndAttack after attackDuration seconds
+    }
+
+    private void EndAttack()
+    {
+        attackHitBox.SetActive(false);
+        isAttacking = false;
+    }
+
+    private void FlipSprite()
+    {
+        if (playerDirection == Vector2.left)
+        {
+            sr.flipX = true;
+        }
+        else if (playerDirection == Vector2.right)
+        {
+            sr.flipX = false;
+        }
     }
 }
