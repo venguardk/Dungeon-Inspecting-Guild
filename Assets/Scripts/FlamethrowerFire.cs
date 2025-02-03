3a1fac8a66479167738d215029d4fa05195dd499
scso-ucsc <scso@ucsc.edu>
2025-02-02 21:23:30 -0800

    Adding Flamethrower Prefab

---
+++ Assets/Scripts/FlamethrowerFire.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class FlamethrowerFire : MonoBehaviour
+{
+    private bool playerInRange;
+    [SerializeField] private int damage = 1;
+    [SerializeField] private float damageInterval = 0.5f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerInRange = true;
+            InvokeRepeating("DamagePlayer", 0f, damageInterval);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            playerInRange = false;
+            CancelInvoke("DamagePlayer");
+        }
+    }
+
+    private void DamagePlayer()
+    {
+        if (playerInRange)
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerManager>().TakeDamage(damage);
+            Debug.Log("Player took damage");
+        }
+    }
+}
