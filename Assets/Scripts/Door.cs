947b2ad270dce696547d4edb280827988490ac2f
scso-ucsc <scso@ucsc.edu>
2025-02-08 00:01:08 -0800

    Adding Door Prefab

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] public bool locked = false;
+    public Collider2D doorCollider;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        doorCollider = GetComponent<Collider2D>();
+        if (locked)
+        {
+            doorCollider.isTrigger = false;
+        }
+        else
+        {
+            doorCollider.isTrigger = true;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && !locked)
+        {
+            Debug.Log("Player entered the door");
+        }
+    }
+}
---
+++ Assets/Scripts/DoorCheck.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DoorCheck : MonoBehaviour
+{
+    [SerializeField] private GameObject door;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && collision.gameObject.GetComponent<PlayerManager>().HasKey())
+        {
+            door.GetComponent<Door>().locked = false;
+            door.GetComponent<Door>().doorCollider.isTrigger = true;
+            collision.gameObject.GetComponent<PlayerManager>().RemoveKey();
+        }
+    }
+}
