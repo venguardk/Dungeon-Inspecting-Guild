20d7b9a5ddc2c90be9bca3eb707f2d001af1a3c9
scso-ucsc <scso@ucsc.edu>
2025-02-15 20:52:39 -0800

    Reworking Flamethrower Prefab

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    [SerializeField] private float maxDistance = 5f;
+    private Vector3 startPosition;
+    private float distanceTravelled;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerManager>().TakeDamage(1);
+            DeactivateFireball();
+        }
+        else if (collision.gameObject.CompareTag("AddedItem") || collision.gameObject.CompareTag("Enemy"))
+        {
+            DeactivateFireball();
+        }
+    }
+
+    public void StartDistanceCount()
+    {
+        startPosition = transform.position;
+        transform.localScale = new Vector3(1f / 3f, 1f / 3f, 1f / 3f);
+
+    }
+
+    private void Update()
+    {
+        distanceTravelled = Vector3.Distance(startPosition, transform.position);
+
+        float scaleMultiplier = Mathf.Lerp(1f / 3f, 1f, distanceTravelled / maxDistance);
+        transform.localScale = new Vector3(scaleMultiplier, scaleMultiplier, scaleMultiplier);
+
+        if (distanceTravelled >= maxDistance)
+        {
+            DeactivateFireball();
+        }
+    }
+
+    private void DeactivateFireball()
+    {
+        this.gameObject.GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+        this.gameObject.SetActive(false);
+        distanceTravelled = 0;
+    }
+}
---
+++ Assets/Scripts/Flamethrower.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flamethrower : MonoBehaviour
+{
+    private List<GameObject> fireballList = new List<GameObject>();
+    [SerializeField] private GameObject fireballPrefab;
+    [SerializeField] private Transform parent, shootPoint;
+    [SerializeField] private float shootSpeed = 2.5f;
+    [SerializeField] private float shootDelay = 0.25f;
+    private bool isShooting;
+
+    private void Start()
+    {
+        Vector3 spawnPoint = Vector3.zero;
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject fireball = Instantiate(fireballPrefab, spawnPoint, Quaternion.Euler(0, 0, 0), parent);
+            fireball.SetActive(false);
+            fireballList.Add(fireball);
+        }
+        isShooting = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (GameManager.instance.IsLevelEditorMode() == false)
+        {
+            if (isShooting == false)
+            {
+                isShooting = true;
+                StartCoroutine(ShootFire());
+            }
+        }
+        else
+        {
+            isShooting = false;
+            StopAllCoroutines();
+        }
+    }
+
+    private IEnumerator ShootFire()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(shootDelay);
+            GameObject chosenFireball = GetFireball();
+            if (chosenFireball != null)
+            {
+                chosenFireball.transform.position = shootPoint.position;
+                chosenFireball.transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z);
+                chosenFireball.SetActive(true);
+                chosenFireball.GetComponent<Rigidbody2D>().linearVelocity = shootPoint.up * shootSpeed;
+                chosenFireball.GetComponent<Fireball>().StartDistanceCount();
+            }
+        }
+    }
+
+    private GameObject GetFireball()
+    {
+        foreach (GameObject fireball in fireballList)
+        {
+            if (!fireball.activeInHierarchy)
+            {
+                return fireball;
+            }
+        }
+        return null;
+    }
+}
