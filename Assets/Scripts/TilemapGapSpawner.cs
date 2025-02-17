a7d0e88e0d8eabe192f4451615bd958c84ec5308
scso-ucsc <scso@ucsc.edu>
2025-02-16 23:14:37 -0800

    Adding Gap Spawning Script

---
+++ Assets/Scripts/TilemapGapSpawner.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapGapSpawner : MonoBehaviour
+{
+    [SerializeField] private Tilemap tilemap;
+    [SerializeField] private GameObject gapPrefab;
+    [SerializeField] private Transform gapsParent;
+
+    void Start()
+    {
+        SpawnGapsOnTilemap();
+    }
+
+    private void SpawnGapsOnTilemap()
+    {
+        BoundsInt bounds = tilemap.cellBounds;
+        for (int x = bounds.xMin; x < bounds.xMax; x++)
+        {
+            for (int y = bounds.yMin; y < bounds.yMax; y++)
+            {
+                Vector3Int cellPosition = new Vector3Int(x, y, 0);
+                if (tilemap.HasTile(cellPosition))
+                {
+                    Vector3 worldPosition = tilemap.CellToWorld(cellPosition);
+                    worldPosition += tilemap.tileAnchor;
+                    GameObject newGap = Instantiate(gapPrefab, worldPosition, Quaternion.identity, gapsParent);
+                }
+            }
+        }
+    }
+}
