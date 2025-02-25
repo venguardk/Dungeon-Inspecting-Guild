e1d819b7b0547a1315649362a3ae3124dfc227ab
Ichiro Sugimoto <isugimot@ucsc.edu>
2025-02-24 19:49:07 -0800

    Merge error fixed

--- Assets/Managers/LevelEditorManager.cs
+++ Assets/Managers/LevelEditorManager.cs
@@ -54,7 +54,6 @@ public class LevelEditorManager : MonoBehaviour, IDataPersistence
         {
             foreach (GameObject obj in allInitialObjects)
             {
-                
                 initialObject.Add(obj);
                 //Separate the initial objects into each dictionary depending on their tags
                 Vector2 initialCoordinate = new Vector2(obj.transform.position.x, obj.transform.position.y);
