b57adce86bc53bbb5fd353eb9b2a996c5a64396e
scso-ucsc <scso@ucsc.edu>
2025-03-02 23:21:06 -0800

    Adding Press & Hold to Reset Button

---
+++ Assets/Scripts/ResetButton.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ResetButton : MonoBehaviour
+{
+    private Image buttonImage;
+    private float pressTime = 0f;
+    private bool isDown = false;
+    [SerializeField] private TextMeshProUGUI buttonText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        buttonImage = GetComponent<Image>();
+        pressTime = 0f;
+        isDown = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDown == true)
+        {
+            pressTime += Time.deltaTime;
+            buttonImage.color = new Color(1f, 1f - pressTime, 1f - pressTime);
+            buttonText.text = "Press and Hold";
+            if (pressTime >= 1f)
+            {
+                SceneLoadManager.instance.RestartScene();
+                pressTime = 0f;
+            }
+        }
+        else
+        {
+            pressTime = 0f;
+            buttonImage.color = Color.white;
+            buttonText.text = "Reset";
+        }
+    }
+
+    public void ButtonDown()
+    {
+        isDown = true;
+    }
+
+    public void ButtonUp()
+    {
+        isDown = false;
+    }
+}
