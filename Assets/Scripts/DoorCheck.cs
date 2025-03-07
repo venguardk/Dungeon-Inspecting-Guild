c3e93651cb072bcb2058000b75e68567426671e6
venguardk <kpaw@ucsc.edu>
2025-03-07 13:26:25 -0800

    added safety zone, fixed door
    - added image for the safety zone. Will probably need some tweaking (temp)
    - added code to remove whole sprite of locked door, when entering key zone

--- Assets/Scripts/DoorCheck.cs
+++ Assets/Scripts/DoorCheck.cs
@@ -11,6 +11,16 @@ public class DoorCheck : MonoBehaviour
             door.GetComponent<Door>().locked = false;
             door.GetComponent<Door>().doorCollider.isTrigger = true;
             door.GetComponent<SpriteRenderer>().enabled = false;
+
+            // adjusting to get rid of locked door sprites
+
+            for (int i = 1; i < door.transform.childCount; i++)
+            {
+                GameObject door_lock = door.transform.GetChild(i).gameObject;
+                door_lock.GetComponent<SpriteRenderer>().enabled = false;
+            }
+
+            // end of additional code
             collision.gameObject.GetComponent<PlayerManager>().RemoveKey();
         }
     }
