1a6bef7200d95b512579920cd4e351a7e9dcdc5e
venguardk <kpaw@ucsc.edu>
2025-03-20 00:44:15 -0700

    removing a lot of fluff from pixel art folder

--- Assets/Pixel Art/Isometric Test.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1" name="Isometric Test" tilewidth="32" tileheight="32" tilecount="152" columns="19">
- <image source="Isometric Blocks.aseprite" width="612" height="256"/>
-</tileset>
