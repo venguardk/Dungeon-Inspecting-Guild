e271d84209b5d0dbd8bc0e32d2cbee1179b24e19
LeCodingOwl <nguyenjimmy627@gmail.com>
2025-04-14 16:19:07 -0700

    Removed checklist comment

--- Assets/Scripts/Gap.cs
+++ Assets/Scripts/Gap.cs
@@ -6,15 +6,6 @@ using System.Threading;
 
 public class Gap : MonoBehaviour
 {
-    // private void OnTriggerEnter2D(Collider2D other)
-    // this will check if the player is on top of the the gap, if so
-    // - prevent player from moving
-    // - show fall indicator
-    // - player loses a heart
-    // - spawn player to a safe spot
-    // if a monster wanders into the gap
-    // - it dies
-
     public bool playerIsFalling = false;
     PlayerManager player;
 
